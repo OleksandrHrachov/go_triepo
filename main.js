8a68be09bbffaf8c2f05dd715763de3efc7f746d
o.hrachov <ol.grachov@gmail.com>
2023-12-29 11:44:52 +0200

    add mobile version for header

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+const MENU_BTN = document.querySelector(".header__menu-btn");
+const NAV = document.querySelector(".header_nav");
+const MENU_BTN_IMG = document.querySelector(".header__menu-btn-img");
+
+if (MENU_BTN) {
+  MENU_BTN.addEventListener("click", () => {
+    NAV.classList.toggle("header_nav--open");
+    MENU_BTN_IMG.classList.toggle('header__menu-btn-img--open');
+  });
+}
+
 const swiper = new Swiper(".swiper", {
   modules: [Navigation, Pagination, EffectCoverflow],
   direction: "horizontal",
@@ -17,8 +28,8 @@ const swiper = new Swiper(".swiper", {
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  
-  effect: 'coverflow',
+
+  effect: "coverflow",
   coverflowEffect: {
     rotate: 0,
     depth: 0,
@@ -30,8 +41,6 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
 });
 
 swiper.init();
-
