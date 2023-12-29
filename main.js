95dd8389c2b2a3a0ad445a2a14e8396e43d3c3a8
o.hrachov <ol.grachov@gmail.com>
2023-12-29 12:31:24 +0200

    fix page scroll with open menu

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import "./style.scss";
 
+const BODY = document.querySelector("body");
+const MENU_BTN = document.querySelector(".header__menu-btn");
+const NAV = document.querySelector(".header_nav");
+const MENU_BTN_IMG = document.querySelector(".header__menu-btn-img");
+
 import Swiper from "swiper";
 import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
 
@@ -7,16 +12,19 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const MENU_BTN = document.querySelector(".header__menu-btn");
-const NAV = document.querySelector(".header_nav");
-const MENU_BTN_IMG = document.querySelector(".header__menu-btn-img");
-
 if (MENU_BTN) {
   MENU_BTN.addEventListener("click", () => {
+    BODY.classList.toggle("page__body--lock");
     NAV.classList.toggle("header_nav--open");
-    MENU_BTN_IMG.classList.toggle('header__menu-btn-img--open');
+    MENU_BTN_IMG.classList.toggle("header__menu-btn-img--open");
   });
 }
+
+window.addEventListener("resize", () => {
+  BODY.classList.remove("page__body--lock");
+  NAV.classList.remove("header_nav--open");
+  MENU_BTN_IMG.classList.remove("header__menu-btn-img--open");
+});
 
 const swiper = new Swiper(".swiper", {
   modules: [Navigation, Pagination, EffectCoverflow],
